4252af06064fcad77b72992fa7b218023ea38d8f
Raviraj Chundawat <ravirajsc@gmail.com>
2025-02-10 14:33:22 +0000

    a simple my map

--- src/ravi/ds/map/CheckMap.java
+++ src/ravi/ds/map/CheckMap.java
@@ -15,8 +15,15 @@ public class CheckMap {
         System.out.println("repeated element in Array1 "+checkMap.checkifExisting(firstArray));
         System.out.println("repeated element in Array2 "+checkMap.checkifExisting(secondArray));
         System.out.println("repeated element in Array3 "+checkMap.checkifExisting(thirdArray));
+
+        System.out.println("repeated element in Array1 "+checkMap.checkifExistiingInloop(firstArray));
+        System.out.println("repeated element in Array2 "+checkMap.checkifExistiingInloop(secondArray));
+        System.out.println("repeated element in Array3 "+checkMap.checkifExistiingInloop(thirdArray));
+
+
     }
 
+    // check if a element comes second time
     public Integer checkifExisting(int[] input){
         HashSet<Integer> elements= new HashSet<>();
         if(input==null || input.length<1) {
@@ -33,5 +40,20 @@ public class CheckMap {
 
         }
         return null;
+    }
+
+    public Integer checkifExistiingInloop(int[] input){
+        if(input==null || input.length<1) {
+            System.out.println("Wrong input");
+        }else {
+            for(int i=1;i<input.length;i++){
+                for(int j=i-1;j>=0;j--){
+                    if(input[i]==input[j]){
+                        return input[i];
+                    }
+                }
+            }
+        }
+            return null;
     }
 }
