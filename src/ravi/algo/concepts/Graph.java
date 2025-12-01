5bda75fca9e7b7aa97bb49b88cee388e09cc3eb8
Raviraj Chundawat <ravirajsc@gmail.com>
2025-12-01 23:34:54 +0000

    graph

--- src/ravi/algo/concepts/Graph.java
+++ src/ravi/algo/concepts/Graph.java
@@ -1,6 +1,7 @@
 package ravi.algo.concepts;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.*;
 
 public class Graph {
 
@@ -31,6 +32,49 @@ public class Graph {
 
     //Network Delay Time
     public static int networkDelayTime(int[][] times, int n, int k) {
+
+         Map<Integer, List<int[]>> adlist=new HashMap<>();
+         for(int[] time:times){
+             int from=time[0];
+             int to=time[1];
+             int cost=time[2];
+             adlist.computeIfAbsent(from,key->new ArrayList<>()).add(new int[]{to,cost});
+         }
+
+         PriorityQueue<int[]> pq=new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
+         pq.offer(new int[]{0,k});
+
+         Set<Integer> visted=new HashSet<>();
+         int maxDelay=0;
+
+         while(!pq.isEmpty()){
+             int[] top=pq.poll();
+             int node=top[1];
+             int time=top[0];
+
+             if(visted.contains(node))
+                 continue;
+
+             visted.add(node);
+
+             maxDelay=Math.max(maxDelay,time);
+
+             List<int[]> niebours=adlist.getOrDefault(node,new ArrayList<>());
+
+             for(int[] nieb:niebours){
+                 int toNieb=nieb[0];
+                 int toCost=nieb[1];
+                 if(!visted.contains(toNieb)) {
+                     int totalCpst = toCost + time;
+                     pq.offer(new int[]{totalCpst,toNieb});
+                 }
+             }
+         }
+
+        if(visted.size()==n){
+            return maxDelay;
+        }
+
 
         // Replace this placeholder return statement with your code
         return -1;
